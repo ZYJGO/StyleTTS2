481d864aae0c182115fb95199188e953b2e462f7
ZYJGO <zyj2323@gmail.com>
2024-07-05 16:41:21 +0200

    test

---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import setup, find_packages, Command
+
+cwd = os.path.dirname(os.path.abspath(__file__))
+
+with open(os.path.join(cwd, 'requirements_setup.txt')) as f:
+    reqs = f.read().splitlines()
+
+class PostInstallCommand(Command):
+
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+
+        os.system('python -m unidic download')
+        os.system('pip install git+https://github.com/resemble-ai/monotonic_align.git')
+
+setup(
+    name='styletts2',
+    version='0.1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=reqs,
+    cmdclass={
+        'install': PostInstallCommand,
+    },
+)
