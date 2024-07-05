baa7dbc7582175e5426c1c5b06e7efe90f2850cf
ZYJGO <zyj2323@gmail.com>
2024-07-05 17:11:49 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -1,27 +1,10 @@
 import os
-import subprocess
-from setuptools import setup, find_packages, Command
+from setuptools import setup, find_packages
 
 cwd = os.path.dirname(os.path.abspath(__file__))
 
-with open(os.path.join(cwd, 'requirements_setup.txt')) as f:
+with open(os.path.join(cwd, 'requirements.txt')) as f:
     reqs = f.read().splitlines()
-
-class PostInstallCommand(Command):
-    user_options = []
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        try:
-            subprocess.check_call(['python', '-m', 'unidic', 'download'])
-            subprocess.check_call(['pip', 'install', 'git+https://github.com/resemble-ai/monotonic_align.git'])
-        except subprocess.CalledProcessError as e:
-            print("Failed to execute a command: ", e)
 
 setup(
     name='styletts2',
@@ -29,7 +12,4 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=reqs,
-    cmdclass={
-        'install': PostInstallCommand,
-    },
 )
