b31631955fdc5c16196485d9abc3bfeb49d43dc3
ZYJGO <zyj2323@gmail.com>
2024-07-08 09:09:13 +0200

    update

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as f:
-    reqs = f.read().splitlines()
+with open('requirements.txt.lock') as f:
+    reqs = f.readlines()
+
+requirements = []
+for req in reqs:
+    req = req.strip()
+    if req and not req.startswith('#'):
+        requirements.append(req.split(' ')[0])
 
 setup(
     name='styletts2',
     version='0.1.0',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=reqs,
+    install_requires=requirements,
 )
